467e80eca5c4d3f81fcabcf3ab41eade6ccc73cf
Madhurendra1311 <kmadhu1311@gmail.com>
2021-09-22 18:21:32 +0530

    sass_learning

---
+++ basic_sass/src/App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+import MainPage from './Components/MainPage';
+import Navbar from './Components/Navbar';
+// import "./Styles/Navbar.scss"
+// import "./Styles/MainPage.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ basic_sass/src/Components/MainPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../Styles/MainPage.scss"
+
+function MainPage() {
+    return (
+        <>
+            <div className="mainPage">
+                <h1>Welcome to the website</h1>
+                <button>Click Here</button>
+            </div>
+            <h1>Hi</h1>
+        </>
+    )
+}
+
+export default MainPage
